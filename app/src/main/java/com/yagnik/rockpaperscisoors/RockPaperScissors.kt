ea10d05e7ef7a219e3ff9291e2967111b4f91997
Yagniklathiya <yagniklathiya181@gmail.com>
2024-05-04 10:09:36 +0530

    this is 1st commit

---
+++ app/src/main/java/com/yagnik/rockpaperscisoors/RockPaperScissors.kt
@@ -0,0 +1,47 @@
+fun main() {
+  var compterChoice = ""
+  var playerChoice = ""
+
+
+
+
+
+  println("Rock,Paper and scissors , Enter your choice ")
+  playerChoice = readln()
+
+  while (playerChoice !in arrayOf("Rock","Paper","Scissor")){
+    println("Your choice is invalid")
+    playerChoice = readln()
+  }
+
+    val randomNumber= (1..3).random()
+
+    when(randomNumber) {
+      1 -> {
+        compterChoice = "Rock"
+      }
+      2 -> {
+        compterChoice = "Paper"
+      }
+      3 -> {
+        compterChoice = "Scissor"
+      }
+
+    }
+    println(compterChoice)
+    val winner = when {
+      playerChoice == compterChoice -> "tie"
+      playerChoice == "Rock" && compterChoice == "Scissor" -> "Player"
+      playerChoice == "Paper" && compterChoice == "Rock" -> "Player"
+      playerChoice == "Scissor" && compterChoice == "Paper" -> "Player"
+      else -> "Computer"
+    }
+      if(winner == "tie"){
+        println("Its a Tie")
+      }
+      else{
+        println(" Congratulations $winner won " )
+      }
+
+
+}
